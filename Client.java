80ffbcb3425a298cb27a562dd4c78d3bf6428d95
Kirtik Singh <kirtiksingh8@gmail.com>
2020-11-27 17:15:44 +0530

    Client Ready for sending DataStream

--- Client.java
+++ Client.java
@@ -1,34 +1,72 @@
+package Socket_CN2020_Kirtik_Singh;
+
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket; 
 
 public class Client{
 
     public static void main(String args[]) throws IOException{
-        
-        // Make a new client side connection
-        Socket clientSocket = new Socket("localhost", 9000);
 
-        // Create an output stream
-        DataOutputStream dataOutput = new DataOutputStream(clientSocket.getOutputStream());
+        // private Socket socket = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // private BufferedReader inputFromServer = null;
+        // private DataOutputStream outputStreamToServer = null;
 
-        // Send data to the server
+        try{
+            // Make a new client side connection
+            Socket clientSocket = new Socket("localhost", 9000);
 
-        // Send the number of nodes and the matrix
+            // Create an output stream  to server
+            DataOutputStream dataOutput = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader dataInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        // int nodes=3;
-        // int[][] adjMatrix = {
-        //     {0, 1, 0},
-        //     {1, 0, 1},
-        //     {0, 0, 0}
-        // };
+            // Taking user input for the data to be sent
+            int size = 5;
+            System.out.println("Enter the matrix to be sent");
 
-        dataOutput.writeInt(nodes);
-        dataOutput.flush();
-        for (int i = 0; i < nodes; i++)
-            for (int j = 0; j < nodes; j++)
-                dataOutput.writeInt(adjMatrix[i][j]);
-                dataOutput.flush();
+            // Send the number of nodes and the matrix to the server
+            dataOutput.writeInt(size);
+            for(int i = 0; i < size; i++){
+                String matrixLine = reader.readLine();
+                String[] numbers = matrixLine.split(" ");
+                for(String num: numbers){
+                    dataOutput.writeInt(Integer.parseInt(num));
+                }
+            }
+            System.out.println("Enter the length of path, or n:");
+            dataOutput.writeChar(Integer.parseInt(reader.readLine()));
+
+            System.out.println("Enter the first node in Letters:");
+            dataOutput.writeChar(reader.readLine().charAt(0));
+
+            System.out.println("Enter the second node in Letters:");
+            dataOutput.writeChar(reader.readLine().charAt(0));
+            reader.close();
+            dataOutput.close();
+            clientSocket.close();
+            
+
+            // int nodes=5;
+            // int[][] adjMatrix = {
+            //     {0, 1, 0},
+            //     {1, 0, 1},
+            //     {0, 0, 0}
+            }
+        catch(Exception e){
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
     }
+
+        // dataOutput.writeInt(nodes);
+        // dataOutput.flush();
+        // for (int i = 0; i < nodes; i++)
+        //     for (int j = 0; j < nodes; j++)
+        //         dataOutput.writeInt(adjMatrix[i][j]);
+        //         dataOutput.flush();    
 }
